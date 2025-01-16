b81a3b68d6fa16be51cfe31599625e6906c89f69
Gian Michele Innocenti <gminnocen@gmail.com>
2025-01-16 13:33:02 -0500

    update input file for MC-based BDT (#76)

--- ML/runML.sh
+++ ML/runML.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 MLoptimization.py --input_file_mc /Users/ginnocen/Desktop/MITHIGAnalysis2024/Skims/SkimsMC/20241216_v1_filelist20241216_Pthat2_ForceD0Decay100M_BeamA_v1/mergedfile.root --tree_name Tree --output_csv output_pt_p1p2_y_m1p1.cvs --output_root output_pt_p1p2_y_m1p1.root --output_model XGBoostMConly_pt_p1p2_y_m1p1.json --ptmin 1 --ptmax 2 --ymin -1 --ymax 1
+python3 MLoptimization.py --input_file_mc /data00/UPCD0LowPtAnalysis_2023ZDCORData_2023reco/SkimsMC/20241216_v1_filelist20241216_Pthat2_ForceD0Decay100M_BeamA_v1/mergedfile.root --tree_name Tree --output_csv output_pt_p1p2_y_m1p1.cvs --output_root output_pt_p1p2_y_m1p1.root --output_model XGBoostMConly_pt_p1p2_y_m1p1.json --ptmin 1 --ptmax 2 --ymin -1 --ymax 1
