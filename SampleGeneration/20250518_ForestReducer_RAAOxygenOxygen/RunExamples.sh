54e505b4318e9c0965492e716819ef2cae996539
Gian Michele Innocenti <gminnocen@gmail.com>
2025-05-28 15:13:49 -0400

    Update input samples (#120)
    * add DD Starlight MC

--- SampleGeneration/20250518_ForestReducer_RAAOxygenOxygen/RunExamples.sh
+++ SampleGeneration/20250518_ForestReducer_RAAOxygenOxygen/RunExamples.sh
@@ -4,19 +4,31 @@ source clean.sh
 rm -rf Output/
 mkdir -p Output/
 ./Execute --Input ${PATHSAMPLE}/crab_HiForest_250520_Hijing_MinimumBias_b015_OO_5362GeV_250518.root \
-   --Output skim_HiForest_250520_Hijing_MinimumBias_b015_OO_5362GeV_250518.root \
+   --Output ${PATHSKIMSAMPLE}/skim_HiForest_250520_Hijing_MinimumBias_b015_OO_5362GeV_250518.root \
    --DoGenLevel false \
    --Year 2025 \
    --IsData false \
-   --Fraction 1 \
+   --Fraction 1. \
    --PFTree particleFlowAnalyser/pftree \
-   --sampleType 0
+   --sampleType 0 \
+   --DebugMode true
 
 ./Execute --Input ${PATHSAMPLE}/crab_HiForest_250514_Starlight_SingleDiffraction_OO_5362GeV_1505PR47944.root \
-   --Output skim_HiForest_250514_Starlight_SingleDiffraction_OO_5362GeV_1505PR47944.root \
+   --Output ${PATHSKIMSAMPLE}/skim_HiForest_250514_Starlight_SingleDiffraction_OO_5362GeV_1505PR47944.root \
    --DoGenLevel false \
    --Year 2025 \
-   --Fraction 1 \
+   --Fraction 1. \
    --IsData false \
    --PFTree particleFlowAnalyser/pftree \
-   --sampleType 1
+   --sampleType 1 \
+   --DebugMode true
+
+./Execute --Input ${PATHSAMPLE}/crab_HiForest_250514_Starlight_DoubleDiffraction_OO_5362GeV_1505PR47944.root \
+   --Output ${PATHSKIMSAMPLE}/skim_HiForest_250514_Starlight_DoubleDiffraction_OO_5362GeV_1505PR47944.root \
+   --DoGenLevel false \
+   --Year 2025 \
+   --Fraction 1. \
+   --IsData false \
+   --PFTree particleFlowAnalyser/pftree \
+   --sampleType 2 \
+   --DebugMode true
