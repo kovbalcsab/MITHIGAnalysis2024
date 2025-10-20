93e4ac5f5d31d4b96c2bee0481470e758d7a7b1b
kovbalcsab <kovacsbcs@gmail.com>
2025-10-20 21:30:50 +0200

    early PPS test

--- MainAnalysis/20251011_PPSDiffraction/loop.C
+++ MainAnalysis/20251011_PPSDiffraction/loop.C
@@ -12,7 +12,7 @@ void loop() {
 
   gStyle->SetOptStat(0);
   // Open the ROOT file
-  TFile *f = TFile::Open("20250703_pO_PhysicsIonPhysics0_393952_full.root");
+  TFile *f = TFile::Open("/data00/bakovacs/OOsamples/Skims/20250703_pO_PhysicsIonPhysics1_393952_full.root");
   // TFile *f = TFile::Open("20250703_pO_PhysicsIonPhysics0_393952_full.root");
   if (!f || f->IsZombie()) {
     std::cerr << "Could not open file!\n";
@@ -30,6 +30,12 @@ void loop() {
   // x axis = number of tracks, y axis = number of events
   TH1F *hNtrNoCond = new TH1F("hNtrNoCond", "; n. HP tracks |#eta|<2.4, p_{T}>0.3 GeV; Entries", 1000, -0.5, 999.5);
   TH1F *hNtrPPS = new TH1F("hNtrPPS", "; n. HP tracks |#eta|<2.4, p_{T}>0.3 GeV; Entries", 1000, -0.5, 999.5);
+  TH1F *htrkPhiNoCond = new TH1F("htrkPhiNoCond", "; track #phi; Entries", 200, -3.15, 3.15);
+  TH1F *htrkPhiPPS = new TH1F("htrkPhiPPS", "; track #phi; Entries", 200, -3.15, 3.15);
+  TH2F *hNtrvsFSC3TOT_chargefCNoCond =
+      new TH2F("hNtrvsFSC3TOT_chargefCNoCond", "; n. HP tracks |#eta|<2.4, p_{T}>0.3 GeV; FSC3 sum [fC]", 100, 0, 1000, 4000, 0, 400000 * 4);
+  TH2F *hNtrvsFSC3TOT_chargefCPPS =
+      new TH2F("hNtrvsFSC3TOT_chargefCPPS", "; n. HP tracks |#eta|<2.4, p_{T}>0.3 GeV; FSC3 sum [fC]", 100, 0, 1000, 4000, 0, 400000 * 4);
   TH2F *hZDCpvsZDCmNoCond = new TH2F("hZDCpvsZDCmNoCond", "; ZDC+; ZDC-", 100, 0, 8000, 100, 0, 8000);
   TH2F *hZDCpvsZDCmPPS = new TH2F("hZDCpvsZDCmPPS", "; ZDC+; ZDC-", 100, 0, 8000, 100, 0, 8000);
   TH1F *hZDCmNoCond = new TH1F("hZDCmNoCond", "; ZDC-; Entries", 100, 0, 8000);
@@ -38,15 +44,41 @@ void loop() {
   TH1F *hZDCpPPS = new TH1F("hZDCpPPS", "; ZDC+; Entries", 100, 0, 8000);
   TH1F *hFSC2bottomM_adcNoCond = new TH1F("hFSC2bottomM_adcNoCond", "; FSC2bottomM_adcNoCond; Entries", 100, 0, 256);
   TH1F *hFSC2bottomM_adcPPS = new TH1F("hFSC2bottomM_adcPPS", "; FSC2bottomM_adcPPS; Entries", 100, 0, 256);
+  TH1F *hFSC3bottomleftM_adcNoCond =
+      new TH1F("hFSC3bottomleftM_adcNoCond", "; FSC3bottomleftM_adcNoCond; Entries", 100, 0, 256);
+  TH1F *hFSC3bottomleftM_adcPPS = new TH1F("hFSC3bottomleftM_adcPPS", "; FSC3bottomleftM_adcPPS; Entries", 100, 0, 256);
   TH1F *hFSC2bottomM_chargefCNoCond =
       new TH1F("hFSC2bottomM_chargefCNoCond", "; FSC2bottomM_chargefCNoCond; Entries", 10000, 0, 400000);
   TH1F *hFSC2bottomM_chargefCPPS =
       new TH1F("hFSC2bottomM_chargefCPPS", "; FSC2bottomM_chargefCPPS; Entries", 10000, 0, 400000);
+  TH1F *hFSC3bottomleftM_chargefCNoCond =
+      new TH1F("hFSC3bottomleftM_chargefCNoCond", "; FSC3bottomleftM_chargefCNoCond; Entries", 10000, 0, 400000);
+  TH1F *hFSC3bottomleftM_chargefCPPS =
+      new TH1F("hFSC3bottomleftM_chargefCPPS", "; FSC3bottomleftM_chargefCPPS; Entries", 10000, 0, 400000);
+  TH2F *hFSC3TOT_adcvsZDCmNoCond = new TH2F("hFSC3TOT_adcvsZDCmNoCond", "; ZDC-; FSC3 sum [ADC]", 100, 0, 8000, 400, 0, 256*4);
+  TH2F *hFSC3TOT_adcvsZDCmPPS = new TH2F("hFSC3TOT_adcvsZDCmPPS", "; ZDC-; FSC3 sum [ADC]", 100, 0, 8000, 400, 0, 256*4);
+  TH2F *hFSC3TOT_chargefCvsZDCmNoCond = new TH2F("hFSC3TOT_chargefCvsZDCmNoCond", "; ZDC-; FSC3 sum [fC]", 100, 0, 8000, 4000, 0, 400000*4);
+  TH2F *hFSC3TOT_chargefCvsZDCmPPS = new TH2F("hFSC3TOT_chargefCvsZDCmPPS", "; ZDC-; FSC3 sum [fC]", 100, 0, 8000, 4000, 0, 400000*4);
+  TH2F *hFSC3TOT_adcvsFSC2TOT_adcNoCond = new TH2F("hFSC3TOT_adcvsFSC2TOT_adcNoCond", "; FSC2 sum [ADC]; FSC3 sum [ADC]", 200, 0, 256*2, 400, 0, 256*4);
+  TH2F *hFSC3TOT_adcvsFSC2TOT_adcPPS = new TH2F("hFSC3TOT_adcvsFSC2TOT_adcPPS", "; FSC2 sum [ADC]; FSC3 sum [ADC]", 200, 0, 256*2, 400, 0, 256*4);
+  TH2F *hFSC3TOT_chargefCvsFSC2TOT_chargefCNoCond = new TH2F("hFSC3TOT_chargefCvsFSC2TOT_chargefCNoCond", "; FSC2 sum [fC]; FSC3 sum [fC]", 2000, 0, 400000*2, 4000, 0, 400000*4);
+  TH2F *hFSC3TOT_chargefCvsFSC2TOT_chargefCPPS = new TH2F("hFSC3TOT_chargefCvsFSC2TOT_chargefCPPS", "; FSC2 sum [fC]; FSC3 sum [fC]", 2000, 0, 400000*2, 4000, 0, 400000*4);
+  TH2F *hFSC3TOT_chargefCvsHFEMaxMinusNoCond = new TH2F("hFSC3TOT_chargefCvsHFEMaxMinusNoCond", "; HFE Max-; FSC3 sum [fC]", 400, 0, 100, 4000, 0, 400000*4);
+  TH2F *hFSC3TOT_chargefCvsHFEMaxMinusPPS = new TH2F("hFSC3TOT_chargefCvsHFEMaxMinusPPS", "; HFE Max-; FSC3 sum [fC]", 400, 0, 100, 4000, 0, 400000*4);
+  TH2F *hFSC3TOT_chargefCvshiHFMinusNoCond = new TH2F("hFSC3TOT_chargefCvshiHFMinusNoCond", "; hiHF-; FSC3 sum [fC]", 200, 0, 200, 4000, 0, 400000*4);
+  TH2F *hFSC3TOT_chargefCvshiHFMinusPPS = new TH2F("hFSC3TOT_chargefCvshiHFMinusPPS", ";; hiHF-; FSC3 sum [fC]", 200, 0, 200, 4000, 0, 400000*4);
+
+  TH1F *hMissingptAngleNoCond = new TH1F("hMissingptAngleNoCond", "; Angle of missing p_{T}; Entries", 100, -3.15, 3.15);
+  TH1F *hMissingptAnglePPS = new TH1F("hMissingptAnglePPS", "; Angle of missing p_{T}; Entries", 100, -3.15, 3.15);
+  TH2F *PPS0xvsy = new TH2F("PPS0xvsy", "; PPSStation0M_x; PPSStation0M_y", 150, -10, 20, 150, -10, 20);
+  TH2F *PPS2xvsy = new TH2F("PPS2xvsy", "; PPSStation2M_x; PPSStation2M_y", 150, -10, 20, 150, -10, 20);
+  TH1F *h2Ddistance = new TH1F("h2Ddistance", "; Distance; Entries", 100, 0, 20);
+  TH2I *hPPS0vsPPS2Size = new TH2I("hPPS0vsPPS2Size", "; PPSStation0M size; PPSStation2M size", 20, 0, 20, 20, 0, 20);
   bool passBaselineEventSelection = false;
   float VZ;
   float VZError;
   float ZDCsumMinus, ZDCsumPlus;
-  float HFEMaxMinus, HFEMaxPlus;
+  float HFEMaxMinus, HFEMaxPlus, hiHFMinus_pf;
   bool HLT_OxyZeroBias_v1 = false;
 
   std::vector<float> *trkPt = nullptr;
@@ -54,7 +86,7 @@ void loop() {
   std::vector<float> *trkPhi = nullptr;
   std::vector<bool> *highPurity = nullptr;
 
-  std::vector<int> *PPSStation0M_x = nullptr;
+  std::vector<float> *PPSStation0M_x = nullptr;
   std::vector<float> *PPSStation0M_y = nullptr;
   std::vector<float> *PPSStation2M_x = nullptr;
   std::vector<float> *PPSStation2M_y = nullptr;
@@ -77,6 +109,7 @@ void loop() {
   t->SetBranchAddress("ZDCsumPlus", &ZDCsumPlus);
   t->SetBranchAddress("HFEMaxMinus", &HFEMaxMinus);
   t->SetBranchAddress("HFEMaxPlus", &HFEMaxPlus);
+  t->SetBranchAddress("hiHFMinus_pf", &hiHFMinus_pf);
 
   t->SetBranchAddress("HLT_OxyZeroBias_v1", &HLT_OxyZeroBias_v1);
 
@@ -106,6 +139,7 @@ void loop() {
   const Long64_t nEntries = t->GetEntries();
   const Long64_t nMax = std::min<Long64_t>(nEntries, 1000000000000); // cap to 1000 as in your code
   int multHighPurity2p4Pt0p3 = 0;
+  std::cerr << "Processing " << nMax << " entries...\n";
 
   for (Long64_t i = 0; i < nMax; ++i) {
     t->GetEntry(i);
@@ -126,20 +160,45 @@ void loop() {
 
     // std::cout << "Event " << i << ": passBaselineEventSelection = " << passBaselineEventSelection << "\n";
 
+    int size_PPSStation0M_x = PPSStation0M_x->size();
+    int size_PPSStation0M_y = PPSStation0M_y->size();
+    int size_PPSStation2M_x = PPSStation2M_x->size();
+    int size_PPSStation2M_y = PPSStation2M_y->size();
+    if (size_PPSStation0M_x != size_PPSStation0M_y) {
+      std::cerr << "Warning: PPSStation0M_x and PPSStation0M_y have different sizes!\n";
+      continue;
+    }
+    if (size_PPSStation2M_x != size_PPSStation2M_y) {
+      std::cerr << "Warning: PPSStation2M_x and PPSStation2M_y have different sizes!\n";
+      continue;
+    }
+    hPPS0vsPPS2Size->Fill(size_PPSStation0M_x, size_PPSStation2M_x);
+    double missingpt_x = 0.0;
+    double missingpt_y = 0.0;
+
     for (size_t j = 0; j < trkPt->size(); ++j) {
       if (highPurity->at(j)) {
         if (trkPt->at(j) > 0.3 && fabs(trkEta->at(j)) < 2.4) {
           multHighPurity2p4Pt0p3++;
+          missingpt_x += trkPt->at(j) * cos(trkPhi->at(j));
+          missingpt_y += trkPt->at(j) * sin(trkPhi->at(j));
+          htrkPhiNoCond->Fill(trkPhi->at(j), trkPt->at(j));
+          if (size_PPSStation0M_x > 0 && size_PPSStation0M_y > 0 && size_PPSStation2M_x > 0 && size_PPSStation2M_y > 0) {
+            htrkPhiPPS->Fill(trkPhi->at(j), trkPt->at(j));
+          }
         }
       }
 
       // std::cout << "  Track " << j << ": Pt = " << trkPt->at(j) << ", Eta = " << trkEta->at(j) << ", Phi = " <<
       // trkPhi->at(j) << "\n";
     }
-    int size_PPSStation0M_x = PPSStation0M_x->size();
-    int size_PPSStation0M_y = PPSStation0M_y->size();
-    int size_PPSStation2M_x = PPSStation2M_x->size();
-    int size_PPSStation2M_y = PPSStation2M_y->size();
+    
+    int FSC3TOT_adc = FSC3bottomleftM_adc->at(2) + FSC3bottomrightM_adc->at(2) + FSC3topleftM_adc->at(2) +
+                      FSC3toprightM_adc->at(2);
+    float FSC3TOT_chargefC = FSC3bottomleftM_chargefC->at(2) + FSC3bottomrightM_chargefC->at(2) +
+                              FSC3topleftM_chargefC->at(2) + FSC3toprightM_chargefC->at(2);
+    int FSC2TOT_adc = FSC2topM_adc->at(2) + FSC2bottomM_adc->at(2);
+    float FSC2TOT_chargefC = FSC2topM_chargefC->at(2) + FSC2bottomM_chargefC->at(2);
     if (size_PPSStation0M_x > 0 && size_PPSStation0M_y > 0 && size_PPSStation2M_x > 0 && size_PPSStation2M_y > 0) {
       hNtrPPS->Fill(multHighPurity2p4Pt0p3);
       hZDCpvsZDCmPPS->Fill(ZDCsumPlus, ZDCsumMinus);
@@ -147,6 +206,21 @@ void loop() {
       hZDCpPPS->Fill(ZDCsumPlus);
       hFSC2bottomM_adcPPS->Fill(FSC2bottomM_adc->at(2));
       hFSC2bottomM_chargefCPPS->Fill(FSC2bottomM_chargefC->at(2));
+      hFSC3bottomleftM_adcPPS->Fill(FSC3bottomleftM_adc->at(2));
+      hFSC3bottomleftM_chargefCPPS->Fill(FSC3bottomleftM_chargefC->at(2));
+      hFSC3TOT_adcvsZDCmPPS->Fill(ZDCsumMinus, FSC3TOT_adc);
+      hFSC3TOT_chargefCvsZDCmPPS->Fill(ZDCsumMinus, FSC3TOT_chargefC);
+      hFSC3TOT_adcvsFSC2TOT_adcPPS->Fill(FSC2TOT_adc, FSC3TOT_adc);
+      hFSC3TOT_chargefCvsFSC2TOT_chargefCPPS->Fill(FSC2TOT_chargefC, FSC3TOT_chargefC);
+      hNtrvsFSC3TOT_chargefCPPS->Fill(multHighPurity2p4Pt0p3, FSC3TOT_chargefC);
+      PPS0xvsy->Fill(PPSStation0M_x->at(0), PPSStation0M_y->at(0));
+      PPS2xvsy->Fill(PPSStation2M_x->at(0), PPSStation2M_y->at(0));
+      float distance =
+          sqrt(pow(PPSStation0M_x->at(0) - PPSStation2M_x->at(0), 2) + pow(PPSStation0M_y->at(0) - PPSStation2M_y->at(0), 2));
+      h2Ddistance->Fill(distance);
+      hMissingptAnglePPS->Fill(atan2(missingpt_y, missingpt_x));
+      hFSC3TOT_chargefCvsHFEMaxMinusPPS->Fill(HFEMaxMinus, FSC3TOT_chargefC);
+      hFSC3TOT_chargefCvshiHFMinusPPS->Fill(hiHFMinus_pf, FSC3TOT_chargefC);
     }
     hNtrNoCond->Fill(multHighPurity2p4Pt0p3);
     hZDCpvsZDCmNoCond->Fill(ZDCsumPlus, ZDCsumMinus);
@@ -154,6 +228,16 @@ void loop() {
     hZDCpNoCond->Fill(ZDCsumPlus);
     hFSC2bottomM_adcNoCond->Fill(FSC2bottomM_adc->at(2));
     hFSC2bottomM_chargefCNoCond->Fill(FSC2bottomM_chargefC->at(2));
+    hFSC3bottomleftM_adcNoCond->Fill(FSC3bottomleftM_adc->at(2));
+    hFSC3bottomleftM_chargefCNoCond->Fill(FSC3bottomleftM_chargefC->at(2));
+    hFSC3TOT_adcvsZDCmNoCond->Fill(ZDCsumMinus, FSC3TOT_adc);
+    hFSC3TOT_chargefCvsZDCmNoCond->Fill(ZDCsumMinus, FSC3TOT_chargefC);
+    hFSC3TOT_adcvsFSC2TOT_adcNoCond->Fill(FSC2TOT_adc, FSC3TOT_adc);
+    hFSC3TOT_chargefCvsFSC2TOT_chargefCNoCond->Fill(FSC2TOT_chargefC, FSC3TOT_chargefC);
+    hNtrvsFSC3TOT_chargefCNoCond->Fill(multHighPurity2p4Pt0p3, FSC3TOT_chargefC);
+    hMissingptAngleNoCond->Fill(atan2(missingpt_y, missingpt_x));
+    hFSC3TOT_chargefCvsHFEMaxMinusNoCond->Fill(HFEMaxMinus, FSC3TOT_chargefC);
+    hFSC3TOT_chargefCvshiHFMinusNoCond->Fill(hiHFMinus_pf, FSC3TOT_chargefC);
   }
 
   TFile *outFile = TFile::Open("output.root", "RECREATE");
@@ -167,8 +251,34 @@ void loop() {
   hZDCpPPS->Write();
   hFSC2bottomM_adcNoCond->Write();
   hFSC2bottomM_adcPPS->Write();
+  hFSC3bottomleftM_adcNoCond->Write();
+  hFSC3bottomleftM_adcPPS->Write();
   hFSC2bottomM_chargefCNoCond->Write();
   hFSC2bottomM_chargefCPPS->Write();
+  hFSC3bottomleftM_chargefCNoCond->Write();
+  hFSC3bottomleftM_chargefCPPS->Write();
+  hFSC3TOT_adcvsZDCmNoCond->Write();
+  hFSC3TOT_adcvsZDCmPPS->Write();
+  hFSC3TOT_chargefCvsZDCmNoCond->Write();
+  hFSC3TOT_chargefCvsZDCmPPS->Write();
+  hFSC3TOT_adcvsFSC2TOT_adcNoCond->Write();
+  hFSC3TOT_adcvsFSC2TOT_adcPPS->Write();
+  hFSC3TOT_chargefCvsFSC2TOT_chargefCNoCond->Write();
+  hFSC3TOT_chargefCvsFSC2TOT_chargefCPPS->Write();
+  hNtrvsFSC3TOT_chargefCNoCond->Write();
+  hNtrvsFSC3TOT_chargefCPPS->Write();
+  PPS0xvsy->Write();
+  PPS2xvsy->Write();
+  h2Ddistance->Write();
+  htrkPhiNoCond->Write();
+  htrkPhiPPS->Write();
+  hMissingptAngleNoCond->Write();
+  hMissingptAnglePPS->Write();
+  hPPS0vsPPS2Size->Write();
+  hFSC3TOT_chargefCvsHFEMaxMinusNoCond->Write();
+  hFSC3TOT_chargefCvsHFEMaxMinusPPS->Write();
+  hFSC3TOT_chargefCvshiHFMinusNoCond->Write();
+  hFSC3TOT_chargefCvshiHFMinusPPS->Write();
   outFile->Close();
   f->Close();
 }
--- MainAnalysis/20251011_PPSDiffraction/plot.C
+++ MainAnalysis/20251011_PPSDiffraction/plot.C
@@ -9,9 +9,9 @@
 #include <iostream>
 #include <vector>
 
-void plot() {
+void plot(char *filename) {
 
-  TFile *f = TFile::Open("output.root");
+  TFile *f = TFile::Open(filename);
   if (!f || f->IsZombie()) {
     std::cerr << "Could not open file!\n";
     return;
@@ -31,7 +31,7 @@ void plot() {
   hNtrNoCond->GetXaxis()->SetTitle("Charged hadron multiplicity, #eta|< 2.4");
   hNtrNoCond->GetXaxis()->SetRangeUser(0, 300);
   hNtrPPS->GetXaxis()->SetRangeUser(0, 300);
-  hNtrNoCond->SetMaximum(0.5);
+  hNtrNoCond->SetMaximum(1.0);
   hNtrNoCond->Draw("same");
   hNtrPPS->Draw("same");
   TLegend *legend = new TLegend(0.4, 0.7, 0.9, 0.9);
@@ -41,7 +41,7 @@ void plot() {
   legend->AddEntry(hNtrNoCond, "No requirements", "l");
   legend->AddEntry(hNtrPPS, "At least 1 proton in PPS", "l");
   legend->Draw();
-  cChargedTracks->SaveAs("cChargedTracks.png");
+  cChargedTracks->SaveAs("cChargedTracks.pdf");
 
   TH1F *hFSC2bottomM_adcPPS = (TH1F *)f->Get("hFSC2bottomM_adcPPS");
   TH1F *hFSC2bottomM_adcNoCond = (TH1F *)f->Get("hFSC2bottomM_adcNoCond");
@@ -57,7 +57,7 @@ void plot() {
   hFSC2bottomM_adcPPS->GetXaxis()->SetTitle("FSC2 bottom ADC");
   hFSC2bottomM_adcPPS->GetXaxis()->SetRangeUser(0, 2000);
   hFSC2bottomM_adcNoCond->GetXaxis()->SetRangeUser(0, 2000);
-  hFSC2bottomM_adcPPS->SetMaximum(0.5);
+  hFSC2bottomM_adcPPS->SetMaximum(1.0);
   hFSC2bottomM_adcPPS->Draw("same");
   hFSC2bottomM_adcNoCond->Draw("same");
   TLegend *legend2 = new TLegend(0.4, 0.7, 0.9, 0.9);
@@ -67,7 +67,7 @@ void plot() {
   legend2->AddEntry(hFSC2bottomM_adcPPS, "At least 1 proton in PPS", "l");
   legend2->AddEntry(hFSC2bottomM_adcNoCond, "No requirements", "l");
   legend2->Draw();
-  cFSC2bottomM->SaveAs("cFSC2bottomM.png");
+  cFSC2bottomM->SaveAs("cFSC2bottomM.pdf");
 
   TH1F *hFSC2bottomM_chargefCNoCond = (TH1F *)f->Get("hFSC2bottomM_chargefCNoCond");
   TH1F *hFSC2bottomM_chargefCPPS = (TH1F *)f->Get("hFSC2bottomM_chargefCPPS");
@@ -94,5 +94,117 @@ void plot() {
   legend3->AddEntry(hFSC2bottomM_chargefCPPS, "At least 1 proton in PPS", "l");
   legend3->AddEntry(hFSC2bottomM_chargefCNoCond, "No requirements", "l");
   legend3->Draw();
-  cFSC2bottomM_chargefC->SaveAs("cFSC2bottomM_chargefC.png");
+  cFSC2bottomM_chargefC->SaveAs("cFSC2bottomM_chargefC.pdf");
+
+  TCanvas *cFSC3bottomleftM = new TCanvas("cFSC3bottomleftM", "cFSC3bottomleftM", 1000, 850);
+  TH1F *hFSC3bottomleftM_adcPPS = (TH1F *)f->Get("hFSC3bottomleftM_adcPPS");
+  TH1F *hFSC3bottomleftM_adcNoCond = (TH1F *)f->Get("hFSC3bottomleftM_adcNoCond");
+  cFSC3bottomleftM->SetLogy();
+  hFSC3bottomleftM_adcPPS->Rebin(4);
+  hFSC3bottomleftM_adcNoCond->Rebin(4);
+  hFSC3bottomleftM_adcPPS->Scale(1. / hFSC3bottomleftM_adcPPS->Integral());
+  hFSC3bottomleftM_adcPPS->SetLineColor(kBlue);
+  hFSC3bottomleftM_adcNoCond->Scale(1. / hFSC3bottomleftM_adcNoCond->Integral());
+  hFSC3bottomleftM_adcNoCond->SetLineColor(kRed);
+  hFSC3bottomleftM_adcPPS->GetXaxis()->SetTitle("FSC3 bottom left ADC");
+  hFSC3bottomleftM_adcPPS->GetXaxis()->SetRangeUser(0, 2000);
+  hFSC3bottomleftM_adcNoCond->GetXaxis()->SetRangeUser(0, 2000);
+  hFSC3bottomleftM_adcPPS->SetMaximum(1.0);
+  hFSC3bottomleftM_adcPPS->Draw("same");
+  hFSC3bottomleftM_adcNoCond->Draw("same");
+  TLegend *legend4 = new TLegend(0.4, 0.7, 0.9, 0.9);
+  legend4->SetBorderSize(0);
+  legend4->SetFillStyle(0);
+  legend4->SetTextSize(0.035);
+  legend4->AddEntry(hFSC3bottomleftM_adcPPS, "At least 1 proton in PPS", "l");
+  legend4->AddEntry(hFSC3bottomleftM_adcNoCond, "No requirements", "l");
+  legend4->Draw();
+  cFSC3bottomleftM->SaveAs("cFSC3bottomleftM.pdf");
+
+  // FSC3 bottom left charge plots
+  TCanvas *cFSC3bottomleftM_chargefC = new TCanvas("cFSC3bottomleftM_chargefC", "cFSC3bottomleftM_chargefC", 1000, 850);
+  TH1F *hFSC3bottomleftM_chargefCPPS = (TH1F *)f->Get("hFSC3bottomleftM_chargefCPPS");
+  TH1F *hFSC3bottomleftM_chargefCNoCond = (TH1F *)f->Get("hFSC3bottomleftM_chargefCNoCond");
+  cFSC3bottomleftM_chargefC->SetLogy();
+  hFSC3bottomleftM_chargefCPPS->GetXaxis()->SetRangeUser(0, 400000);
+  hFSC3bottomleftM_chargefCNoCond->GetXaxis()->SetRangeUser(0, 400000);
+  hFSC3bottomleftM_chargefCPPS->Rebin(10);
+  hFSC3bottomleftM_chargefCNoCond->Rebin(10);
+  hFSC3bottomleftM_chargefCPPS->Scale(1. / hFSC3bottomleftM_chargefCPPS->Integral());
+  hFSC3bottomleftM_chargefCPPS->SetLineColor(kBlue);
+  hFSC3bottomleftM_chargefCNoCond->Scale(1. / hFSC3bottomleftM_chargefCNoCond->Integral());
+  hFSC3bottomleftM_chargefCNoCond->SetLineColor(kRed);
+  hFSC3bottomleftM_chargefCPPS->GetXaxis()->SetTitle("FSC3 bottom left charge [fC]");
+  hFSC3bottomleftM_chargefCPPS->SetMaximum(4.0);
+  hFSC3bottomleftM_chargefCPPS->Draw();
+  hFSC3bottomleftM_chargefCNoCond->Draw("same");
+  TLegend *legend5 = new TLegend(0.4, 0.7, 0.9, 0.9);
+  legend5->SetBorderSize(0);
+  legend5->SetFillStyle(0);
+  legend5->SetTextSize(0.035);
+  legend5->AddEntry(hFSC3bottomleftM_chargefCPPS, "At least 1 proton in PPS", "l");
+  legend5->AddEntry(hFSC3bottomleftM_chargefCNoCond, "No requirements", "l");
+  legend5->Draw();
+  cFSC3bottomleftM_chargefC->SaveAs("cFSC3bottomleftM_chargefC.pdf");
+
+  // ZDC- plots
+  TCanvas *cZDCm = new TCanvas("cZDCm", "cZDCm", 1000, 850);
+  TH1F *hZDCmPPS = (TH1F *)f->Get("hZDCmPPS");
+  TH1F *hZDCmNoCond = (TH1F *)f->Get("hZDCmNoCond");
+  cZDCm->SetLogy();
+  hZDCmPPS->Rebin(4);
+  hZDCmNoCond->Rebin(4);
+  hZDCmPPS->Scale(1. / hZDCmPPS->Integral());
+  hZDCmPPS->SetLineColor(kBlue);
+  hZDCmNoCond->Scale(1. / hZDCmNoCond->Integral());
+  hZDCmNoCond->SetLineColor(kRed);
+  hZDCmPPS->GetXaxis()->SetTitle("ZDC-");
+  hZDCmPPS->GetXaxis()->SetRangeUser(0, 8000);
+  hZDCmNoCond->GetXaxis()->SetRangeUser(0, 8000);
+  hZDCmPPS->SetMaximum(1.0);
+  hZDCmPPS->Draw();
+  hZDCmNoCond->Draw("same");
+  TLegend *legend6 = new TLegend(0.4, 0.7, 0.9, 0.9);
+  legend6->SetBorderSize(0);
+  legend6->SetFillStyle(0);
+  legend6->SetTextSize(0.035);
+  legend6->AddEntry(hZDCmPPS, "At least 1 proton in PPS", "l");
+  legend6->AddEntry(hZDCmNoCond, "No requirements", "l");
+  legend6->Draw();
+  cZDCm->SaveAs("cZDCm.pdf");
+
+  // ZDC+ plots
+  TCanvas *cZDCp = new TCanvas("cZDCp", "cZDCp", 1000, 850);
+  TH1F *hZDCpPPS = (TH1F *)f->Get("hZDCpPPS");
+  TH1F *hZDCpNoCond = (TH1F *)f->Get("hZDCpNoCond");
+  cZDCp->SetLogy();
+  hZDCpPPS->Rebin(4);
+  hZDCpNoCond->Rebin(4);
+  hZDCpPPS->Scale(1. / hZDCpPPS->Integral());
+  hZDCpPPS->SetLineColor(kBlue);
+  hZDCpNoCond->Scale(1. / hZDCpNoCond->Integral());
+  hZDCpNoCond->SetLineColor(kRed);
+  hZDCpPPS->GetXaxis()->SetTitle("ZDC+");
+  hZDCpPPS->GetXaxis()->SetRangeUser(0, 8000);
+  hZDCpNoCond->GetXaxis()->SetRangeUser(0, 8000);
+  hZDCpPPS->SetMaximum(1);
+  hZDCpPPS->Draw();
+  hZDCpNoCond->Draw("same");
+  TLegend *legend7 = new TLegend(0.4, 0.7, 0.9, 0.9);
+  legend7->SetBorderSize(0);
+  legend7->SetFillStyle(0);
+  legend7->SetTextSize(0.035);
+  legend7->AddEntry(hZDCpPPS, "At least 1 proton in PPS", "l");
+  legend7->AddEntry(hZDCpNoCond, "No requirements", "l");
+  legend7->Draw();
+  cZDCp->SaveAs("cZDCp.pdf");
+}
+
+int main(char **argv, char **argc) {
+  if (argc < 2) {
+    std::cerr << "Please provide the input filename as an argument.\n";
+    return 1;
+  }
+  plot(argv[1]);
+  return 0;
 }
