046bfc05e9a620c6c31c83f28fd7814f20c9f265
kovbalcsab <kovacsbcs@gmail.com>
2025-10-27 15:02:59 +0100

    Add macro for merging outputs

---
+++ SampleGeneration/20251023_ForestReducer_pODiffraction/MergeOutputFiles.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+FOLDER_IDX=(1 2 3 4 5)
+FILE_IDX=(1 2 3 4 5 6 7 8 9)
+# Merging ROOT files using hadd
+
+for folder in "${FOLDER_IDX[@]}"; do
+    for file in "${FILE_IDX[@]}"; do
+        input_files="/data00/bakovacs/pOsamples/Skims/output_2025102*_Skim_test_pO_sample_000${folder}/output_${file}*.root"
+        output_file="/data00/bakovacs/pOsamples/Skims/merged_skims/merged_output_${folder}_${file}.root"
+        if [ -f "$output_file" ]; then
+            echo "Output file $output_file already exists. Skipping merge."
+            continue
+        fi
+        
+        # Add output_0.root to 1st file index
+        if [ "$file" -eq 1 ]; then
+            input_files="/data00/bakovacs/pOsamples/Skims/output_2025102*_Skim_test_pO_sample_000${folder}/output_0.root $input_files"
+        fi
+        hadd $output_file $input_files
+    done
+done
+echo "Merging completed."
